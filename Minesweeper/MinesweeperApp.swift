8c47a2793f5b157d929a2ea74b21264e4c243d03
Philipp <pd95@users.noreply.github.com>
2024-08-08 13:10:44 +0200

    Initial Commit

---
+++ Minesweeper/MinesweeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  MinesweeperApp.swift
+//  Minesweeper
+//
+//  Created by Philipp on 08.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct MinesweeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
